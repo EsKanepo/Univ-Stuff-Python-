7032f17e7b7607a83a0ec2319a06a7855b01138a
EsKanepo <ethanwilbert48@gmail.com>
2025-03-19 14:23:08 +0700

    Update yay python.py

--- yay python.py
+++ yay python.py
@@ -1,16 +1,3 @@
-##arr = [7, 1, 4, 8, 10, 3, 2]  
-##maks = arr[0]
-##
-##for i in arr:
-##    if i > maks:
-##        maks = i
-##
-##print("Bilangan terbesar adalah", maks)
-##
-##Tn(max) = O(1)+O(1)+(O(n)*O(1)*O(1))+O(1)
-##Tn(max) = O(1),O(1),O(n),O(1)
-##Tn(max) = O(n)
-
 arr = [7, 1, 4, 8, 10, 3, 2]
 
 for i in range(len(arr)):
@@ -19,7 +6,3 @@ for i in range(len(arr)):
             arr[i], arr[j] = arr[j], arr[i]
 
 print(arr)
-
-##Tn(max) =(O(1), O(n^2), O(1), O(1))
-##Tn(max)= O(n^2)
-
