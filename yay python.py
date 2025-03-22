98e4f29ee11b4d405e6b9405fd8e47ddfcd1ca9f
EsKanepo <ethanwilbert48@gmail.com>
2025-03-22 17:56:44 +0700

    Update yay python.py

--- yay python.py
+++ yay python.py
@@ -11,3 +11,22 @@ for c in hitung:
 Tn(max) = O(1)+O(1)+(O(n)*O(1))+(O(n)*O(1))
 Tn(max) = O(1),O(1),O(n),O(n)
 Tn(max) = O(n)
+
+#No 2
+text = input("Masukkan teks : ").split()
+huruf = []
+
+for i in range(len(text)):
+    for j in range(len(text[i])):
+        huruf.append(text[i][j])
+
+for i in range(len(huruf)):
+    for j in range(len(huruf)-i-1):
+        if ord(huruf[j]) > ord(huruf[j+1]):
+            huruf[j],huruf[j+1] = huruf[j+1] , huruf[j]
+
+print(" ".join(huruf))
+
+Tn(max) = O(1)+O(1)+(O(n)*O(n)*O(1))+(O(n)*O(n)*O(1)*O(1))+O(1)
+Tn(max) = O(1),O(1),O(n^2),O(n^2),O(1)
+Tn(max) = O(n^2)
