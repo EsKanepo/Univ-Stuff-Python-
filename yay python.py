14ea9475a8b3c32123c5f70e181455576ae28580
EsKanepo <ethanwilbert48@gmail.com>
2025-03-24 01:36:13 +0700

    Update yay python.py

--- yay python.py
+++ yay python.py
@@ -30,3 +30,40 @@ print(" ".join(huruf))
 Tn(max) = O(1)+O(1)+(O(n)*O(n)*O(1))+(O(n)*O(n)*O(1)*O(1))+O(1)
 Tn(max) = O(1),O(1),O(n^2),O(n^2),O(1)
 Tn(max) = O(n^2)
+
+#No 3
+def perkalian_matriks(m1, m2):
+    baris1 = len(m1)
+    baris2 = len(m2)
+    kolom1 = len(m1[0])
+    kolom2 = len(m2[0])
+    hasil = [[0 for _ in range(kolom2)] for _ in range(baris1)]
+ 
+    for i in range(baris1):
+        for j in range(kolom2):
+            for k in range(kolom1):
+                hasil[i][j] += m1[i][k] * m2[k][j]
+    return hasil
+ 
+def data_matriks(baris, kolom):
+    m = []
+    for i in range(baris):
+        baris_matriks = list(map(int, input().split()))
+        m.append(baris_matriks)
+    return m
+ 
+ 
+N = int(input("Masukkan Untuk Kolom dan Baris : "))
+ 
+print("Matriks 1 :")
+m1 = data_matriks(N, N)
+print("Matriks 2 :")
+m2 = data_matriks(N, N)
+ 
+hasil = perkalian_matriks(m1, m2)
+for baris in hasil:
+    print(baris)
+
+Tn(max)= (O(1)*O(1)+O(1)+O(1)+O(1)+O(1)+(O(n)*O(n)*O(n))+O(1))+O(1)*O(1)+O(n)*O(1)+O(1)+O(1)+O(1)
+Tn(max)= O(n^3)
+
