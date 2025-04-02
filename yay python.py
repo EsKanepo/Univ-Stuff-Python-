419dee37c5851d4cd1759d1ca410e99c532f4b0d
EsKanepo <ethanwilbert48@gmail.com>
2025-04-03 00:37:48 +0700

    Update yay python.py

--- yay python.py
+++ yay python.py
@@ -1,69 +1,56 @@
 #No 1
-n = list(map(int,input("Masukkan angka : ").split()))
-
-hitung = [0] * 10
-for i in n:
-    hitung[i] += 1
-
-for c in hitung:
-    print(c)
-
-Tn(max) = O(1)+O(1)+(O(n)*O(1))+(O(n)*O(1))
-Tn(max) = O(1),O(1),O(n),O(n)
-Tn(max) = O(n)
+##n = input("Masukkan 8 koin : ")
+##koin = list(n)
+##idx = 1
+##
+##i = ord(koin[0])
+##if koin[0] == koin[1]:
+##    for i in range(2,8):
+##        if koin[i]!= koin[0]:
+##            idx = i+1
+##            break
+##elif koin[0] == koin[2]:
+##    idx = 2
+##
+##else:
+##    idx = 1
+##
+##print(idx)
+##
+##Tn(max) : O(1)+O(1)+O(1)+O(1)+O(1)*(O(n)+O(1)*(O(1)+O(1)))+O(1)*O(1)+O(1)*O(1)+O(1)
+##Tn(max) : O(1),O(n),O(1),O(1)
+##Tn(max) : O(n)
 
 #No 2
-text = input("Masukkan teks : ").split()
-huruf = []
+def cari(pattern, text):
+    if pattern in text:
+        return "Ketemu"
+    return "Tidak Ketemu"
+pattern_input = str(input("Pattern : ")).lower()
+text_input = str(input("Text : ")).lower()
+print(cari(pattern_input, text_input))
 
-for i in range(len(text)):
-    for j in range(len(text[i])):
-        huruf.append(text[i][j])
-
-for i in range(len(huruf)):
-    for j in range(len(huruf)-i-1):
-        if ord(huruf[j]) > ord(huruf[j+1]):
-            huruf[j],huruf[j+1] = huruf[j+1] , huruf[j]
-
-print(" ".join(huruf))
-
-Tn(max) = O(1)+O(1)+(O(n)*O(n)*O(1))+(O(n)*O(n)*O(1)*O(1))+O(1)
-Tn(max) = O(1),O(1),O(n^2),O(n^2),O(1)
-Tn(max) = O(n^2)
+Tn(max) : O(1)+O(1)+O(1)+O(1)
+Tn(max) : O(1),O(1),O(1),O(1)
+Tn(max) : O(1)
 
 #No 3
-def perkalian_matriks(m1, m2):
-    baris1 = len(m1)
-    baris2 = len(m2)
-    kolom1 = len(m1[0])
-    kolom2 = len(m2[0])
-    hasil = [[0 for _ in range(kolom2)] for _ in range(baris1)]
- 
-    for i in range(baris1):
-        for j in range(kolom2):
-            for k in range(kolom1):
-                hasil[i][j] += m1[i][k] * m2[k][j]
-    return hasil
- 
-def data_matriks(baris, kolom):
-    m = []
-    for i in range(baris):
-        baris_matriks = list(map(int, input().split()))
-        m.append(baris_matriks)
-    return m
- 
- 
-N = int(input("Masukkan Untuk Kolom dan Baris : "))
- 
-print("Matriks 1 :")
-m1 = data_matriks(N, N)
-print("Matriks 2 :")
-m2 = data_matriks(N, N)
- 
-hasil = perkalian_matriks(m1, m2)
-for baris in hasil:
-    print(baris)
-
-Tn(max)= (O(1)*O(1)+O(1)+O(1)+O(1)+O(1)+(O(n)*O(n)*O(n))+O(1))+O(1)*O(1)+O(n)*O(1)+O(1)+O(1)+O(1)
-Tn(max)= O(n^3)
-
+##def prime(n):
+##    if n<2:
+##        return False
+##    for i in range(2, int(n**0.5)+1):
+##        if n % i == 0:
+##            return False
+##    else:
+##        return True
+## 
+##angka = list(map(int,input('Masukkan bilangan acak : ').split()))
+##x = 0
+##for i in angka :
+##    if prime(i):
+##        x += 1
+##print(x)
+##
+##Tn(max) : O(n)+O(1)+O(1)+O(1)
+##Tn(max) : O(n),O(1),O(1),O(1)
+##Tn(max) : O(n)
