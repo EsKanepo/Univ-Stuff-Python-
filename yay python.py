828b726f8eab83d37fa2a687f25d0331f2d597ef
EsKanepo <ethanwilbert48@gmail.com>
2025-04-23 12:57:49 +0700

    Update yay python.py

--- yay python.py
+++ yay python.py
@@ -21,3 +21,18 @@
 ##Tn(max) : O(1),O(n),O(1),O(1)
 ##Tn(max) : O(n)
 
+##def jumlah_genap(arr):
+##    if len(arr) == 0:
+##        return 0
+##    elif len(arr) == 1:
+##        if arr[0] % 2 == 0:
+##            return arr[0]
+##        else:
+##            return 0
+##    else:
+##        mid = len(arr)//2
+##        left = jumlah_genap(arr[:mid])
+##        right = jumlah_genap(arr[mid:])
+##        return left + right
+##arr = [5, 2, 7, 8, 6, 3, 4]
+##print(jumlah_genap(arr))
