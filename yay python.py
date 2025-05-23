c2b29fa0c6b18075afffb88a25f8930dfd9b39cb
EsKanepo <ethanwilbert48@gmail.com>
2025-05-24 06:23:11 +0700

    Update yay python.py

--- yay python.py
+++ yay python.py
@@ -1,19 +1,35 @@
-def pangkat(x,y):
-    if y == 0:
-        hasil = 1
-    elif y == 1:
-        hasil = x
-    else:
-        hasil1 = pangkat(x,y//2)       #o log n
-        if y % 2 == 0:
-            hasil = hasil1*hasil1
-        else:
-            hasil = hasil1 * hasil1 * x
-    print(f"x: {x**(y//2)} n/2 : {y//2}")
-    return hasil
-
-a, b = map(int, input().split())
-hasil = pangkat(a,b)
-print("Hasil akhir:", hasil)
-
-#Tn : O(log n)
+def matriks(N, dimesi, Q):
+    dp = []
+    countopt = []
+    countall = []
+    for x in range(N):
+        dp.append([0] * N)
+        countopt.append([0] * N)
+        countall.append([0] * N)
+    for i in range(N):
+            countopt[i][i] = 1
+            countall[i][i] = 1
+    for panjang in range(2, N + 1):
+        for i in range(N - panjang + 1):
+            j = i + panjang - 1
+            dp[i][j] = float('inf')
+            for k in range(i, j):
+                cost = dp[i][k] + dp[k+1][j] + dimensi[i] * dimensi[k+1] * dimensi[j+1]
+                totalall = (countall[i][k] * countall[k+1][j]) % 26101991
+                countall[i][j] = (countall[i][j] + totalall) % 26101991
+                if cost < dp[i][j]:
+                    dp[i][j] = cost
+                    countopt[i][j] = (countopt[i][k] * countopt[k+1][j]) % 26101991
+                elif cost == dp[i][j]:  
+                    countopt[i][j] = (countopt[i][j] + (countopt[i][k] * countopt[k+1][j]) % 26101991) % 26101991
+    if Q == 1:
+        return dp[0][N - 1]
+    elif Q == 2:
+        return countopt[0][N - 1]
+    elif Q == 3:
+        return countll[0][N - 1]
+    
+N = int(input())
+dimensi = list(map(int, input().split()))
+Q = int(input())
+print(matriks(N, dimensi, Q))
